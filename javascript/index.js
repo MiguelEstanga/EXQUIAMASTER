e190b7d9ed1efcf09334eccb2f4911adf08473a1
MiguelEstanga <miguelestanga13@gmail.com>
2023-09-16 10:59:49 -0400

    efecto tactil

--- javascript/index.js
+++ javascript/index.js
@@ -22,6 +22,77 @@ document.addEventListener("DOMContentLoaded"  , function(){
 		Email.click()
 	} )
 
+	window.addEventListener('deviceorientation', handleOrientation);
 
+	function handleOrientation(event) {
+	  // Get the rotation value around the z-axis (tilt left or right)
+	  const tilt = event.gamma;
+	  var alpha = event.alpha;
+	  var gamma = event.gamma;
+	  var beta = event.beta;
+	  // Update the image's CSS transform property to tilt it
+	  const image = document.getElementById('portada');
+	  const textArea = document.getElementById("text_Area")
+	
+	 	imgP = textArea.style.backgroundPosition ;
+	  
+
+	    // Determinar la orientación del dispositivo
+		  var orientation = '';
+		  if (gamma < -45) {
+		    orientation = 'vertical';
+		  } else if (gamma > 45) {
+		    orientation = 'vertical invertida';
+		  } else if (beta < -45) {
+		    orientation = 'horizontal invertida';
+		  } else if (beta > 45) {
+		    orientation = 'horizontal';
+		  }
+
+		  // Determinar la posición de la pantalla
+		  var position = '';
+		  if (orientation === 'vertical') {
+		   
+		     	if( alpha < 180 ){
+		  			textArea.style.backgroundPosition= 'left' 
+		  			image.style.backgroundPosition= 'left' 
+			  	}else{
+			  		textArea.style.backgroundPosition= 'right'
+			  		image.style.backgroundPosition= 'right'
+			  	}
+		  } else if (orientation === 'vertical invertida') {
+		  	if(  alpha < 180 ){
+		  		textArea.style.backgroundPosition = 'right' 
+		  		image.style.backgroundPosition = 'right' 
+		  	}else{
+		  		 textArea.style.backgroundPosition= 'left'
+		  		 image.style.backgroundPosition= 'left'
+		  	}
+		  
+
+		  } else if (orientation === 'horizontal') {
+		  	if( beta < 0 ){
+		  		textArea.style.backgroundPosition= 'left' 
+		  		image.style.backgroundPosition= 'left' 
+		  	}else{
+		  		textArea.style.backgroundPosition = 'right'
+		  		image.style.backgroundPosition = 'right'
+		  	}
+		   
+		  } else if (orientation === 'horizontal invertida') {
+		  	if( beta < 0 ){
+		  		textArea.style.backgroundPosition = 'right'
+		  		image.style.backgroundPosition = 'right'
+		  	}else{
+		  		textArea.style.backgroundPosition = 'left';
+		  		image.style.backgroundPosition = 'left'
+		  	}
+		    
+		  }
+		 
+		  // Hacer algo con la posición de la pantalla
+		  //console.log('La pantalla está hacia la', position);
+
+	}		
 })
 
